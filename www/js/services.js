abbc6de91f3821a8423967c16343ce1960cbe841
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-07-26 04:27:28 -0300

    downgrade ionic version - it wasnt showing the messages

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,6 @@
 angular.module('bucketList.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
        var base = "http://examples-bucketlist.herokuapp.com";
-       //var base = "http://localhost:9804";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
@@ -13,7 +12,7 @@ angular.module('bucketList.services', [])
         };
 
         $rootScope.hide = function () {
-            $ionicLoading.hide();
+            $rootScope.loading.hide();
         };
 
         $rootScope.logout = function () {
